28a951f20288159c02847a69bbb452901ca86d87
奇同 <leo.hx@antfin.com>
2020-08-17 10:42:09 +0800

    grpc

--- grpc/greet_service.h
+++ grpc/greet_service.h
@@ -4,7 +4,23 @@
 
 #ifndef CPP_WX_GREET_SERVIC_H
 #define CPP_WX_GREET_SERVIC_H
+#include<iostream>
+#include <memory>
+#include <string>
 
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+
+#include "grpc/helloworld.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using helloworld::HelloRequest;
+using helloworld::HelloReply;
+using helloworld::Greeter;
 /**
  * 定义类
  */
