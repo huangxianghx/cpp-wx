2b9f0edba8fe7941c6b219cd2a2575614cd65da0
奇同 <leo.hx@antfin.com>
2020-08-17 10:38:29 +0800

    grpc

---
+++ grpc/greet_service.h
@@ -0,0 +1,12 @@
+//
+// Created by 黄湘 on 2020/8/17.
+//
+
+#ifndef CPP_WX_GREET_SERVIC_H
+#define CPP_WX_GREET_SERVIC_H
+
+/**
+ * 定义类
+ */
+class GreeterServiceImpl{};
+#endif //CPP_WX_GREET_SERVIC_H
