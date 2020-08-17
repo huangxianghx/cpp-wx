1d09072f91b05a82f94d2943f2794a2a30d0c4ff
奇同 <leo.hx@antfin.com>
2020-08-17 10:57:45 +0800

    grpc

--- grpc/grpc_server.cc
+++ grpc/grpc_server.cc
@@ -21,6 +21,17 @@ using helloworld::HelloRequest;
 using helloworld::HelloReply;
 using helloworld::Greeter;
 
+/**
+ * grpc stub
+ */
+class GreeterServiceImpl final : public Greeter::Service {
+    Status SayHello(ServerContext* context, const HelloRequest* request,
+                    HelloReply* reply) override {
+        std::string prefix("Hello ");
+        reply->set_message(prefix + request->name());
+        return Status::OK;
+    }
+};
 
 
 void RunServer() {
